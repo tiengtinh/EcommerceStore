0262025621d665a1aebc29b98b17c02d32223a64
Tinh Ngo <tiengtinh@gmail.com>
2018-03-23 16:26:25 +0700

    render by watch product event

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -26,91 +26,102 @@ let _$products
 
 window.App = {
   start: async function() {
-    var self = this;
+    try {
+      var self = this;
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    EcommerceStore.setProvider(web3.currentProvider);
-    EcommerceStore.web3.eth.defaultAccount = web3.eth.accounts[0]
+      // Bootstrap the MetaCoin abstraction for Use.
+      EcommerceStore.setProvider(web3.currentProvider);
+      EcommerceStore.web3.eth.defaultAccount = web3.eth.accounts[0]
 
-    console.log('Get the initial account balance')
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function (err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.")
-        return
+      console.log('Get the initial account balance')
+      // Get the initial account balance so it can be displayed.
+      web3.eth.getAccounts(function (err, accs) {
+        if (err != null) {
+          alert("There was an error fetching your accounts.")
+          return
+        }
+
+        if (accs.length == 0) {
+          alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+          return
+        }
+
+        accounts = accs
+        account = accounts[0]
+      })
+
+      console.log('binding page events')
+
+      let reader
+      $('#image').on('change', function (event) {
+        const file = event.target.files[0]
+        reader = new window.FileReader()
+        reader.readAsArrayBuffer(file)
+      })
+
+      $('#product-create-form').submit((event) => {
+        event.preventDefault()
+
+        this.createProduct(reader).catch(err => console.log('createProduct error: ', err))
+      })
+
+      _$products = $('.products')
+
+      if (_$products.length !== 0) {
+        await this.getProducts()
       }
 
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return
-      }
-
-      accounts = accs
-      account = accounts[0]
-    })
-
-    console.log('binding page events')
-
-    let reader
-    $('#image').on('change', function (event) {
-      const file = event.target.files[0]
-      reader = new window.FileReader()
-      reader.readAsArrayBuffer(file)
-    })
-
-    $('#product-create-form').submit((event) => {
-      event.preventDefault()
-
-      this.createProduct(reader).catch(err => console.log('createProduct error: ', err))
-    })
-
-    _$products = $('.products')
-
-    this.listenContractEvents().catch(err => console.error('listenContractEvents error: ', err))
-
-    if (_$products.length !== 0) {
-      this.getProducts().catch(err => console.error('getProducts error: ', err))
+      await this.listenContractEvents()
+    } catch (err) {
+      console.error('app error: ', err)
     }
   },
 
-  async getProducts() {
+  async getProducts () {
     const response = await fetch('/api/products')
     const resp = await response.json()
-    _products = resp.data
+    _products = resp.data.map((p) => {
+      p.id = p._id
+      delete p._id
+      return p
+    })
 
     this.renderProducts()
   },
 
-  renderStatusBadge(status) {
+  renderStatusBadge (status) {
     switch (status.toString()) {
       case '0': return `<span class="badge badge-danger">Sold</span>`
       case '1': return `<span class="badge badge-primary">Unsold</span>`
       case '2': return `<span class="badge badge-warning">Buying</span>`
-      default: throw 'not recognized status: ' + status.toString()
+      default: throw new Error('not recognized status: ' + status.toString())
     }
   },
 
-  renderProducts() {
+  renderProduct (product) {
+    return `
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://localhost:8080/ipfs/${product.imageLink}" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">${product.name}</a>
+            </h4>
+            <h5>${web3.fromWei(product.price, 'ether')} ETH</h5>
+            <p class="card-text">${product.desc}</p>
+          </div>
+          <div class="card-footer">
+            <span class="badge badge-secondary">${product.category}</span>
+            ${this.renderStatusBadge(product.status)}
+          </div>
+        </div>
+      </div>`
+  },
+
+  renderProducts () {
     _$products.empty()
     for (const product of _products) {
-      _$products.append(`
-        <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://localhost:8080/ipfs/${product.imageLink}" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">${product.name}</a>
-              </h4>
-              <h5>${web3.fromWei(product.price, 'ether')} ETH</h5>
-              <p class="card-text">${product.desc}</p>
-            </div>
-            <div class="card-footer">
-              <span class="badge badge-secondary">${product.category}</span>
-              ${this.renderStatusBadge(product.status)}
-            </div>
-          </div>
-        </div>`
-      )
+      _$products.append(this.renderProduct(product))
     }
   },
 
@@ -124,6 +135,17 @@ window.App = {
     const productCreatedEvent = instance.ProductCreated()
     productCreatedEvent.watch((err, result) => {
       console.log('productCreatedEvent', err, result)
+
+      const product = result.args
+
+      const foundIndex = _products.findIndex((p) => p.id.toString() === result.args.id.toString())
+      if (foundIndex === -1) {
+        _products.push(product)
+      } else {
+        _products[foundIndex] = product
+      }
+
+      this.renderProducts()
     })
   },
 
